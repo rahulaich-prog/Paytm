968c128908972772918cc32c6b00561a2492ca37
rahulaich-prog <aichrahul8906@gmail.com>
2025-08-09 00:45:01 +0530

    changes

--- backend/middleware.js
+++ backend/middleware.js
@@ -30,10 +30,10 @@ const handleTransfer = async () => {
     }
 };
 
-// Update the button onClick
-<button onClick={handleTransfer} className="...">
-    Initiate Transfer
-</button>
+// REMOVE any lines like this:
+// <button onClick={handleTransfer} className="...">
+//     Initiate Transfer
+// </button>
 
 module.exports = {
     authMiddleware
